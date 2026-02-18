ee5c1168a91ba6ce35e06f79a3c4bd71d9b39588
Matthew Furkert <matthew@furkert.com>
2026-02-18 15:33:32 -0500

    switched to libgpiod v2

--- GpioHal/realgpiopin.cpp
+++ GpioHal/realgpiopin.cpp
@@ -1,58 +1,74 @@
-// RealGpioPin.cpp
 #include "realgpiopin.h"
 #include <QDebug>
-
-#ifdef USE_REAL_GPIO
-#include <gpiod.h>
-#endif
 
 namespace Gpio {
 
 RealGpioPin::RealGpioPin(unsigned int offset, QObject* parent)
     : IGpioPin(offset, parent)
+    , m_offset(offset)
 {
-#ifdef USE_REAL_GPIO
-    m_chip = gpiod_chip_open_by_name("gpiochip0");  // change chip name if needed
+    m_chip = gpiod_chip_open("/dev/gpiochip0"); // change to /dev/gpiochip1 /dev/gpiochip2 if needed on your STM32MP15
     if (!m_chip) {
-        qWarning() << "Failed to open gpiochip0";
+        qWarning() << "Failed to open /dev/gpiochip0";
         return;
     }
-    m_line = gpiod_chip_get_line(m_chip, offset);
-    if (!m_line || gpiod_line_request_output(m_line, "gpio_hal_hello", 0) < 0) {
-        qWarning() << "Failed to request GPIO line" << offset;
+
+    // === libgpiod v2: full modern setup === (unchanged)
+    struct gpiod_line_settings *settings = gpiod_line_settings_new();
+    gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_OUTPUT);
+    gpiod_line_settings_set_output_value(settings, GPIOD_LINE_VALUE_INACTIVE);
+
+    struct gpiod_line_config *line_cfg = gpiod_line_config_new();
+    gpiod_line_config_add_line_settings(line_cfg, &m_offset, 1, settings);
+
+    struct gpiod_request_config *req_cfg = gpiod_request_config_new();
+    gpiod_request_config_set_consumer(req_cfg, "gpio_hal_hello");
+
+    m_request = gpiod_chip_request_lines(m_chip, req_cfg, line_cfg);
+
+    // clean up temporary objects
+    gpiod_line_settings_free(settings);
+    gpiod_line_config_free(line_cfg);
+    gpiod_request_config_free(req_cfg);
+
+    if (!m_request) {
+        qWarning() << "Failed to request GPIO line" << offset << "as output";
+        gpiod_chip_close(m_chip);
+        m_chip = nullptr;
     } else {
-        qDebug() << "Real GPIO" << offset << "requested (libgpiod)";
+        qDebug() << "Real GPIO" << offset << "requested successfully (libgpiod v2)";
     }
-#else
-    qWarning() << "RealGpioPin compiled without USE_REAL_GPIO - this should never happen";
-#endif
 }
 
 RealGpioPin::~RealGpioPin()
 {
-#ifdef USE_REAL_GPIO
-    if (m_line) gpiod_line_release(m_line);
-    if (m_chip) gpiod_chip_close(m_chip);
-#endif
+    if (m_request) {
+        gpiod_line_request_release(m_request);
+        m_request = nullptr;
+    }
+    if (m_chip) {
+        gpiod_chip_close(m_chip);
+        m_chip = nullptr;
+    }
 }
 
 bool RealGpioPin::value() const
 {
-#ifdef USE_REAL_GPIO
-    return m_line ? gpiod_line_get_value(m_line) : false;
-#else
-    return false;
-#endif
+    if (!m_request)
+        return false;
+
+    return gpiod_line_request_get_value(m_request, m_offset) == GPIOD_LINE_VALUE_ACTIVE;
 }
 
 void RealGpioPin::setValue(bool value)
 {
-#ifdef USE_REAL_GPIO
-    if (m_line) {
-        gpiod_line_set_value(m_line, value ? 1 : 0);
-        emit valueChanged(value);
+    if (m_request) {
+        enum gpiod_line_value new_val = value ? GPIOD_LINE_VALUE_ACTIVE : GPIOD_LINE_VALUE_INACTIVE;
+        if (gpiod_line_request_set_value(m_request, m_offset, new_val) < 0)
+            qWarning() << "Failed to set GPIO value on line" << m_offset;
+        else
+            emit valueChanged(value);
     }
-#endif
 }
 
 } // namespace Gpio
--- GpioHal/realgpiopin.h
+++ GpioHal/realgpiopin.h
@@ -1,7 +1,6 @@
-// RealGpioPin.h
 #pragma once
 #include "igpiopin.h"
-#include <gpiod.h>   // only included when USE_REAL_GPIO
+#include <gpiod.h>
 
 namespace Gpio {
 
@@ -11,14 +10,14 @@ class RealGpioPin : public IGpioPin
 public:
     explicit RealGpioPin(unsigned int offset, QObject* parent = nullptr);
     ~RealGpioPin() override;
-
     bool value() const override;
     void setValue(bool value) override;
     bool isReal() const override { return true; }
 
 private:
     struct gpiod_chip* m_chip = nullptr;
-    struct gpiod_line* m_line = nullptr;
+    struct gpiod_line_request* m_request = nullptr;
+    unsigned int m_offset = 0;
 };
 
 } // namespace Gpio
